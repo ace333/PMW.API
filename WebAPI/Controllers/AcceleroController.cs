5b61b71bb11b92b67c70e1266100fe6336eb8663
ace333 <achudy333@gmail.com>
2017-11-22 21:56:56 +0100

    small fix to accelero post

--- WebAPI/Controllers/AcceleroController.cs
+++ WebAPI/Controllers/AcceleroController.cs
@@ -30,38 +30,32 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost]
-        public void Post([FromBody]IList<double[]> values)
+        public void Post([FromBody]double[] values)
         {
             var acc = _acceleroRepository.CreateNewItem();
 
-            if(values[0].Length == 5)
+            if(values.Length == 15)
             {
-                acc.XAccelero_1 = values[0][0];
-                acc.XAccelero_2 = values[0][1];
-                acc.XAccelero_3 = values[0][2];
-                acc.XAccelero_4 = values[0][3];
-                acc.XAccelero_5 = values[0][4];
-            }
-            
-            if(values[1].Length == 5)
-            {
-                acc.YAccelero_1 = values[1][0];
-                acc.YAccelero_2 = values[1][1];
-                acc.YAccelero_3 = values[1][2];
-                acc.YAccelero_4 = values[1][3];
-                acc.YAccelero_5 = values[1][4];
-            }
-            
-            if(values[2].Length == 5)
-            {
-                acc.ZAccelero_1 = values[2][0];
-                acc.ZAccelero_2 = values[2][1];
-                acc.ZAccelero_3 = values[2][2];
-                acc.ZAccelero_4 = values[2][3];
-                acc.ZAccelero_5 = values[2][4];
-            }
+                acc.XAccelero_1 = values[0];
+                acc.XAccelero_2 = values[1];
+                acc.XAccelero_3 = values[2];
+                acc.XAccelero_4 = values[3];
+                acc.XAccelero_5 = values[4];
+                acc.YAccelero_1 = values[5];
+                acc.YAccelero_2 = values[6];
+                acc.YAccelero_3 = values[7];
+                acc.YAccelero_4 = values[8];
+                acc.YAccelero_5 = values[9];
+                acc.ZAccelero_1 = values[10];
+                acc.ZAccelero_2 = values[11];
+                acc.ZAccelero_3 = values[12];
+                acc.ZAccelero_4 = values[13];
+                acc.ZAccelero_5 = values[14];
 
-            _acceleroRepository.Insert(acc);
+
+                _acceleroRepository.Insert(acc);
+            }
+            
         }
     }
 }
